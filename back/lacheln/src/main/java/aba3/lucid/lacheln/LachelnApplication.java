b5264ca37ed70f51db205adccf60c4903f0c1c5e
son0925 <shs00925@naver.com>
2025-02-06 15:09:09 +0900

    project start

---
+++ back/lacheln/src/main/java/aba3/lucid/lacheln/LachelnApplication.java
@@ -0,0 +1,13 @@
+package aba3.lucid.lacheln;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LachelnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LachelnApplication.class, args);
+	}
+
+}
---
+++ back/lacheln/src/test/java/aba3/lucid/lacheln/LachelnApplicationTests.java
@@ -0,0 +1,13 @@
+package aba3.lucid.lacheln;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LachelnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

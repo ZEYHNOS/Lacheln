e6fabfcb758408fff63652a88b589dbf732be743
신현준 <sshj1215@gmail.com>
2025-02-07 17:57:37 +0900

    create-hashtag-shin
    required process finished

--- back/lacheln/model/src/main/java/aba3/lucid/hashtag/dto/HashTagDto.java
+++ back/lacheln/model/src/main/java/aba3/lucid/hashtag/dto/HashTagDto.java
@@ -1,10 +1,12 @@
 package aba3.lucid.hashtag.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class HashTagDto {
--- back/lacheln/model/src/main/java/aba3/lucid/hashtag/model/HashTagEntity.java
+++ back/lacheln/model/src/main/java/aba3/lucid/hashtag/model/HashTagEntity.java
@@ -13,10 +13,10 @@ import lombok.NoArgsConstructor;
 @Entity(name = "hashtag")
 public class HashTagEntity {
 
-    // 태그 ID(tagName + pdId로 복합키 사용 예정)
+    // TODO 태그 ID(tagName + pdId로 복합키 사용 예정) --> EmbeddedId 사용?
     @Id
     private String tagName;
 
-    // 외래키
+    // TODO 외래키 설정
     private Long pdId;
 }

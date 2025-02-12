6ba815d09f278f7300e4d0e1c9fc2447b787ed6a
son0925 <shs00925@naver.com>
2025-02-12 17:26:21 +0900

    ws, was 연결 완료

--- back/lacheln/ws/src/main/java/aba3/lucid/controller/company/CompanyController.java
+++
@@ -1,7 +0,0 @@
-package aba3.lucid.controller.company;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class CompanyController {
-}
--- back/lacheln/ws/src/main/java/aba3/lucid/controller/user/UserController.java
+++
@@ -1,9 +0,0 @@
-package aba3.lucid.controller.user;
-
-import org.springframework.web.bind.annotation.RestController;
-
-// 토큰이 무조건 있어야함(이미 로그인이 되어 있는 상태)
-@RestController
-public class UserController {
-
-}

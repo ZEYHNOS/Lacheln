82731e9093b2274036ff5f758fbdf9459c8c8249
shahid11-01 <shahid.ganbarov@gmail.com>
2025-02-18 14:23:14 +0900

    report entity 만들기

--- back/lacheln/model/src/main/java/aba3/lucid/report/ReportEntity.java
+++ back/lacheln/model/src/main/java/aba3/lucid/report/ReportEntity.java
@@ -16,24 +16,35 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @Builder
 public class ReportEntity {
-
+    //TOdo
+    //Entity 만들기
+    //type, column 제대로 만들기
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "report_id", nullable = false)
     private long reportId;
 
+    @Column(name = "user_id", length = 36, nullable = false)
     private String userId;
 
+    @Column(name = "cp_id", nullable = false)
     private long cpId;
 
+    @Column(name = "report_title", length = 100, nullable = false)
     private String reportTitle;
 
+    @Column(name = "report_content", columnDefinition = "TEXT", nullable = false)
     private String reportContent;
 
-    private LocalDateTime createAt;
+    @Column(name = "report_created_at", nullable = false, updatable = false)
+    private LocalDateTime reportCreatedAt;
 
+    @Column(name = "report_target", length = 1, nullable = false)
     private String reportTarget;
 
+
     @Enumerated(EnumType.STRING)
+    @Column(name = "report_category", length = 50, nullable = false)
     private ReportCategory reportCategory;
 
 }

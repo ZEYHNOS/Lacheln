dbdedd2be554d01e31c13a8899de8aa5d46415b8
adina <jackt0506@naver.com>
2025-02-18 17:05:18 +0900

    enum
    enum

--- back/lacheln/model/src/main/java/aba3/lucid/Users/UsersEntity.java
+++ back/lacheln/model/src/main/java/aba3/lucid/Users/UsersEntity.java
@@ -1,10 +1,7 @@
 package aba3.lucid.Users;
 
 import com.fasterxml.uuid.Generators;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -28,14 +25,17 @@ public class UsersEntity {
     private String userId;
 
     @NotNull
+    @Enumerated(EnumType.STRING)
     @Column(name = "country_id", columnDefinition = "CHAR(2)", nullable = false)
     private CountryEnum countryId; //국가명코드 ISO 4217
 
     @NotNull
+    @Enumerated(EnumType.STRING)
     @Column(name = "user_language", columnDefinition = "CAHR(3)", nullable = false)
     private LanguageEnum userLanguage; //국가언어고유코드 ISO 639 alpha-3
 
     @NotNull
+    @Enumerated(EnumType.STRING)
     @Column(name = "user_currency", columnDefinition = "CAHR(3)", nullable = false)
     private CurrencyEnum userCurrency; //화폐단위 ISO 4217
 
@@ -59,6 +59,7 @@ public class UsersEntity {
     private LocalDate userBirthday;
 
     @NotNull
+    @Enumerated(EnumType.STRING)
     @Column(name = "user_social", columnDefinition = "CHAR(1)", nullable = false)
     private SocialEnum userSocial; //로그인 수단 식별 로컬, 카카오, 구글, 애플(L, K, G, A)
 
@@ -70,14 +71,17 @@ public class UsersEntity {
     private String userProfile; //기본 이미지 넣을 예정
 
     @NotNull
+    @Enumerated(EnumType.STRING)
     @Column(name = "user_tier", columnDefinition = "CHAR(30)", nullable = false)
     private TierEnum userTier; //챌린저,월드클래스,프로페셔널,세미프로,아마추어,admin
 
     @NotNull
+    @Enumerated(EnumType.STRING)
     @Column(name = "user_ads_notification", columnDefinition = "CHAR(1)", nullable = false)
     private NotificationEnum userAdsNotification; //알림 동의 여부 Y, N
 
     @NotNull
+    @Enumerated(EnumType.STRING)
     @Column(name = "user_account_status", columnDefinition = "CHAR(20)", nullable = false)
     private AccountStatusEnum userAccountStatus; //활성화, 비활성화, 휴면, 정지
 
@@ -94,6 +98,7 @@ public class UsersEntity {
     private LocalDateTime userAccessTime; //휴면,해킹 체크용 마지막 접속일
 
     @NotNull
+    @Enumerated(EnumType.STRING)
     @Column(name = "user_gender", columnDefinition = "CHAR(1)", nullable = false)
     private GenderEnum userGender; //남자 M  여자 F
 

f1cbcc41a3b5279884e95a57dbcb73f96384585b
신현준 <sshj1215@gmail.com>
2025-03-18 17:50:15 +0900

    create refresh token
    is good...

--- back/lacheln/ws/src/main/java/aba3/lucid/jwtconfig/repository/CompanyRefreshTokenRepository.java
+++ back/lacheln/ws/src/main/java/aba3/lucid/jwtconfig/repository/CompanyRefreshTokenRepository.java
@@ -1,13 +1,13 @@
 package aba3.lucid.jwtconfig.repository;
 
 import aba3.lucid.jwtconfig.CompanyRefreshToken;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface CompanyRefreshTokenRepository extends JpaRepository<CompanyRefreshToken, Long> {
+public interface CompanyRefreshTokenRepository extends CrudRepository<CompanyRefreshToken, Long> {
     Optional<CompanyRefreshToken> findByCpId(Long CompanyId);
     Optional<CompanyRefreshToken> findByRefreshToken(String refreshToken);
 }

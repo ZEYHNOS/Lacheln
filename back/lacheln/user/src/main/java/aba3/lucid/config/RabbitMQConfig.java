fd6b71ce495a107b54065de541a612f8944dc5bd
신현준 <sshj1215@gmail.com>
2025-04-03 16:31:58 +0900

    Feature rabbitmq enviornment
    well done

--- back/lacheln/user/src/main/java/aba3/lucid/config/RabbitMQConfig.java
+++ back/lacheln/user/src/main/java/aba3/lucid/config/RabbitMQConfig.java
@@ -9,12 +9,22 @@ public class RabbitMQConfig {
 
     @Bean
     public Queue toCompanyQueue()   {
-        return new Queue("to.company.queue");
+        return new Queue("to.company");
+    }
+
+    @Bean
+    public Queue companyQueue()   {
+        return new Queue("company");
     }
 
     @Bean
     public DirectExchange toCompanyExchange()   {
-        return new DirectExchange("to.company.exchange");
+        return new DirectExchange("company.exchange");
+    }
+
+    @Bean
+    public Binding companyBinding(Queue companyQueue, DirectExchange toCompanyExchange)   {
+        return BindingBuilder.bind(companyQueue).to(toCompanyExchange).with("company");
     }
 
     @Bean
@@ -24,16 +34,26 @@ public class RabbitMQConfig {
 
     @Bean
     public Queue toUserQueue()  {
-        return new Queue("to.user.queue");
+        return new Queue("to.user");
+    }
+
+    @Bean
+    public Queue userQueue()    {
+        return new Queue("user");
     }
 
     @Bean
     public DirectExchange toUserExchange()   {
-        return new DirectExchange("to.user.exchange");
+        return new DirectExchange("user.exchange");
     }
 
     @Bean
     public Binding toUserBinding(Queue toUserQueue, DirectExchange toUserExchange)   {
         return BindingBuilder.bind(toUserQueue).to(toUserExchange).with("to.user");
+    }
+
+    @Bean
+    public Binding userBinding(Queue userQueue, DirectExchange toUserExchange)   {
+        return BindingBuilder.bind(userQueue).to(toUserExchange).with("user");
     }
 }

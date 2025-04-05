f9ec6ec8ad5d4dc7ba43a973a91e7fc859ca65bf
shahid11-01 <shahid.ganbarov@gmail.com>
2025-04-06 02:41:39 +0900

    calendarconvertor

--- back/lacheln/model/src/main/java/aba3/lucid/domain/calendar/convertor/CalendarConvertor.java
+++ back/lacheln/model/src/main/java/aba3/lucid/domain/calendar/convertor/CalendarConvertor.java
@@ -1,4 +1,41 @@
 package aba3.lucid.domain.calendar.convertor;
 
+
+import aba3.lucid.domain.calendar.dto.CalendarDetailResponse;
+import aba3.lucid.domain.calendar.dto.CalendarRequest;
+import aba3.lucid.domain.calendar.dto.CalendarResponse;
+import aba3.lucid.domain.calendar.entity.CalendarEntity;
+import org.springframework.stereotype.Component;
+
+@Component
 public class CalendarConvertor {
+
+    public CalendarEntity toEntity(CalendarRequest request) {
+        if(request == null) {
+            return null;
+        }
+
+        return CalendarEntity.builder()
+                .calDate(request.getDate())
+                .build();
+    }
+
+    public CalendarResponse toResponse(CalendarEntity entity) {
+        if(entity == null) {
+            return null;
+        }
+
+        return CalendarResponse.builder()
+                .calId(entity.getCalId())
+                .calDate(entity.getCalDate())
+                .companyId(entity.getCompany().getCpId())
+//                .details(List<CalendarDetailResponse>))
+                .build();
+
+    }
+
+
+
+
+
 }

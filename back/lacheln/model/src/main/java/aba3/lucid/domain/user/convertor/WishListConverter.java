e4440646a908e6e74221348981b817ecd5872e99
신현준 <sshj1215@gmail.com>
2025-04-09 12:27:18 +0900

    Feature update Listener
    good

--- back/lacheln/model/src/main/java/aba3/lucid/domain/user/convertor/WishListConverter.java
+++ back/lacheln/model/src/main/java/aba3/lucid/domain/user/convertor/WishListConverter.java
@@ -1,16 +1,29 @@
 package aba3.lucid.domain.user.convertor;
 
 import aba3.lucid.common.annotation.Converter;
+import aba3.lucid.domain.company.entity.CompanyEntity;
 import aba3.lucid.domain.user.dto.WishListDto;
 import aba3.lucid.domain.user.entity.WishListEntity;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Converter
 public class WishListConverter {
 
+    public List<WishListDto> wishListToWishListDto(List<WishListEntity> wishListEntityList) {
+        List<WishListDto> wishListDtoList = new ArrayList<>();
+        for (WishListEntity wishListEntity : wishListEntityList) {
+            WishListDto wishListDto = new WishListDto();
+
+        }
+    }
+
+    public WishListDto wishToWishDto(WishListEntity wishListEntity) {
+
+    }
 
 }

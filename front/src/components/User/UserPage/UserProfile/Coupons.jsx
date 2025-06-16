f449456c5bedb8bdec5776ad1692a4a18674de7c
zeyhnos <zeyhnos@g.yju.ac.kr>
2025-06-17 01:50:15 +0900

    유저페이지 다크모드 수정
    유저페이지 다크모드 수정

--- front/src/components/User/UserPage/UserProfile/Coupons.jsx
+++ front/src/components/User/UserPage/UserProfile/Coupons.jsx
@@ -102,7 +102,7 @@ const CouponPage = () => {
               className={`flex-1 text-center py-4 px-4 font-medium border-b-2 
                 ${location.pathname === tab.path 
                     ? 'border-purple-600 text-purple-600 bg-pp text-white text-md'
-                    : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300'
+                    : 'border-transparent bg-white text-gray-500 hover:text-gray-700 hover:border-gray-300'
                 }`}
             >
               {tab.name}
--- front/src/components/User/UserPage/UserProfile/PaymentAndReview.jsx
+++ front/src/components/User/UserPage/UserProfile/PaymentAndReview.jsx
@@ -286,7 +286,7 @@ const PaymentAndReview = () => {
               className={`flex-1 text-center py-4 px-4 font-medium border-b-2 
                 ${location.pathname === tab.path 
                   ? 'border-purple-600 text-purple-600 bg-pp text-white text-md'
-                    : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300'
+                    : 'border-transparent bg-white text-gray-500 hover:text-gray-700 hover:border-gray-300'
                 }`}
             >
               {tab.name}
@@ -391,7 +391,7 @@ const PaymentAndReview = () => {
                           )}
                         </>
                       )}
-                      <button className="px-4 py-2 border border-gray-300 rounded-lg hover:bg-gray-50">상세보기</button>
+                      <button className="px-4 py-2 border bg-white text-gray-500 border-gray-300 rounded-lg hover:bg-gray-50">상세보기</button>
                     </div>
                   </div>
                 </div>

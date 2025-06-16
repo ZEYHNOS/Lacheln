2a702e917053e29f809d43521799255caf09f271
zeyhnos <zeyhnos@g.yju.ac.kr>
2025-06-17 01:51:30 +0900

    수정
    수정

--- front/src/components/User/UserPage/UserProfile/PaymentAndReview.jsx
+++ front/src/components/User/UserPage/UserProfile/PaymentAndReview.jsx
@@ -312,6 +312,7 @@ const PaymentAndReview = () => {
               className={`flex-1 text-center py-4 px-4 font-medium border-b-2 
                 ${location.pathname === tab.path 
                   ? 'border-purple-600 text-purple-600 bg-pp text-white text-md'
+                    : 'border-transparent bg-white text-gray-500 hover:text-gray-700 hover:border-gray-300'
                 }`}
             >
               {tab.name}

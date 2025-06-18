ad23176ba3d0e2f4d56e7455d37034cb825dfa5b
신현준 <sshj1215@gmail.com>
2025-06-18 18:13:23 +0900

    fff
    ffff

--- back/lacheln/user/src/main/java/aba3/lucid/UserApplication.java
+++ back/lacheln/user/src/main/java/aba3/lucid/UserApplication.java
@@ -2,7 +2,9 @@ package aba3.lucid;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class UserApplication {
     public static void main(String[] args) {
